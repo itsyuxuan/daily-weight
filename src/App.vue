d103f7d9f54212592174a1e4dcec4c754638bfcd
Yuxuan Lin <yuxuan.l@icloud.com>
2022-07-23 21:48:10 +0800

    ✨ feat(App): visualize weight

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,9 @@ const weightChartEl = ref(null)
 const weightChart = shallowRef(null)
 const weightInput = ref(60.0)
 const currentWeight = computed(() => {
-  return weights.value.slice(0).sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+  return (
+    weights.value.slice(0).sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+  )
 })
 
 const addWeight = () => {
@@ -16,6 +18,52 @@ const addWeight = () => {
     date: new Date().getTime(),
   })
 }
+
+watch(
+  weights,
+  (newWeights) => {
+    const ws = [...newWeights]
+    if (weightChart.value) {
+      weightChart.value.data.labels = ws
+        .sort((a, b) => a.date - b.date)
+        .map(w => new Date(w.date).toLocaleDateString())
+        .slice(-7)
+      weightChart.value.data.datasets[0].data = ws
+        .sort((a, b) => a.date - b.date)
+        .map(w => w.weight)
+        .slice(-7)
+      weightChart.value.update()
+      return
+    }
+
+    nextTick(() => {
+      weightChart.value = new Chart(
+        weightChartEl.value.getContext('2d'), {
+          type: 'line',
+          data: {
+            labels: ws
+              .sort((a, b) => a.date - b.date)
+              .map(w => new Date(w.date).toLocaleDateString()),
+            datasets: [
+              {
+                label: 'Weight',
+                data: ws.sort((a, b) => a.date - b.date).map(w => w.weight),
+                backgroundColor: 'rgba(255, 105, 180, 0.2)',
+                borderColor: 'rgb(255, 105, 180)',
+                borderWidth: 1,
+                fill: true,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+          },
+        })
+    })
+  },
+  { deep: true },
+)
 </script>
 
 <template>
@@ -34,22 +82,21 @@ const addWeight = () => {
     <div v-if="weights && weights.length > 0">
       <h2>Last 7 days</h2>
       <div class="canvas-box">
-        <canvas ref="weightCharEl" />
+        <canvas ref="weightChartEl" />
+      </div>
 
-        <div class="weight-history">
-          <h2>Weight History</h2>
-          <ul>
-            <li v-for="weight in weights" :key="weight.weight">
-              <span>{{ weight.weight }}kg</span>
-              <small>{{ new Date(weight.date).toLocaleDateString() }}</small>
-            </li>
-          </ul>
-        </div>
+      <div class="weight-history">
+        <h2>Weight History</h2>
+        <ul>
+          <li v-for="weight in weights" :key="weight.weight">
+            <span>{{ weight.weight }}kg</span>
+            <small>{{ new Date(weight.date).toLocaleDateString() }}</small>
+          </li>
+        </ul>
       </div>
     </div>
   </main>
 </template>
 
-<style scoped>
+<style scoped></style>
 
-</style>
